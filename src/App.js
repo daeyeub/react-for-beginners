57b5be899aa47e58490edcfae5c791e330bbee76
daeyeub <97010621+daeyeub@users.noreply.github.com>
2022-03-04 22:44:33 +0900

    2022-03-04 pm 10

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import Button from "./Button";
-import styles from "./App.module.css"
+import { useState } from "react";
 
-function App() {
-  return (
-   <div>
-    <h1 className={styles.title}>Welcome Back!!!</h1>
-    <Button text={"Continue"}/>
+function App(){
+  const [toDo, setToDo]=useState("");
+  const [toDos, setToDos]=useState([]);
+  const onChange=(event)=>setToDo(event.target.value);
+  const onSubmit=(event)=>{
+    event.preventDefault();
+    if(toDo===""){
+      return;
+    }
+    setToDos((currentArray)=>[toDo, ...currentArray]);
+    setToDo("")
+  }
+  
+  return(
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input value={toDo} onChange={onChange} type="text" placeholder="Write to do.."/>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index)=>(<li key={index}>{item}</li>))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default App
